40c0a4e4f65a6d83586ce00a5a56c697320a8f99
suyashagarwall <suyashagarwall@github.com>
2017-08-14 18:28:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/suyash/bhulekh/Splashscreen.java
@@ -0,0 +1,39 @@
+package com.example.suyash.bhulekh;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends Activity {
+
+    private Thread splashthread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+
+        splashthread = new Thread(){
+
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                Intent intent = new Intent();
+                intent.setClass(Splashscreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        splashthread.start();
+
+    }
+}
